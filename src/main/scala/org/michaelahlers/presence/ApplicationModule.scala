86df1f681339d3a36525c1e907311b55caf230a6
Michael Ahlers <michael@ahlers.co>
2015-08-30 19:00:26 -0400

    Add dumb module to log the app. version and revision.

---
+++ src/main/scala/org/michaelahlers/presence/ApplicationModule.scala
@@ -0,0 +1,17 @@
+package org.michaelahlers.presence
+
+import javax.inject._
+
+import com.google.inject.AbstractModule
+import com.typesafe.scalalogging.LazyLogging
+
+@Singleton
+class ApplicationModule
+  extends AbstractModule
+          with LazyLogging {
+
+  override def configure() = {
+    logger.info( s"""Started Presence version ${BuildInfo.version} (revision ${BuildInfo.revision}).""")
+  }
+
+}
