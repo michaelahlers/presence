f168cb037bd40f87b9403ee6309c3e2befd4f0ac
Michael Ahlers <michael@ahlers.co>
2015-09-20 19:17:42 -0400

    Add first-cut webpack configurations.

---
+++ client-web/webpack.production.config.js
@@ -0,0 +1,75 @@
+'use strict';
+
+var glob = require('glob')
+  , path = require('path')
+  , webpack = require('webpack');
+
+var modulesPath = path.join(__dirname, 'node_modules')
+  , reactPath = path.join(modulesPath, 'react', 'react.js');
+
+var sourcePath = path.join(__dirname, 'src', 'main')
+  , targetPath = path.join(__dirname, 'dist');
+
+module.exports = {
+
+  entry: {
+    index: [
+       path.resolve(sourcePath, 'index.html')
+      , path.resolve(sourcePath, 'index.js')
+    ],
+    vendors: ['react']
+  },
+
+  resolve: {
+    alias: {
+      'react': reactPath
+    }
+  },
+
+  output: {
+    path: targetPath,
+    filename: '[name].js'
+  },
+
+  module: {
+
+    preLoaders: [
+      {
+        test: /\.js$/,
+        loader: 'eslint',
+        exclude: /node_modules/
+      }
+    ],
+
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: modulesPath,
+        loader: 'babel',
+        query: {
+          /* Enable ES7 features. */
+          stage: 0,
+          cacheDirectory: true
+        },
+        noParse: ['react']
+      }, {
+        test: /\.html$/,
+        /* TODO: Switch to URL loader. */
+        loader: 'file',
+        query: {
+          context: sourcePath,
+          name: '[path][name].[ext]'
+        }
+      }, {
+        test: /\.less$/,
+        loader: 'style!css!less?strictMath' //&noIeCompat'
+      }
+    ]
+
+  },
+
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin('vendors', 'vendors.js')
+  ]
+
+};
