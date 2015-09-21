5f81bada368dc730b6639da9fb929b8cc1c9cfba
Michael Ahlers <michael@ahlers.co>
2015-09-20 20:33:54 -0400

    Add a dump demonstration of workings CORS.

--- client-web/src/main/ahlers/michael/presence/client/web/index.js
+++ client-web/src/main/ahlers/michael/presence/client/web/index.js
@@ -3,11 +3,21 @@
 require('./index.less');
 
 import React from 'react';
+import restful from 'restful.js';
 
 const content =
   <div>
     <h1>Presence</h1>
     <p>Hello world!</p>
   </div>;
+
+restful('localhost')
+  .port(9000)
+  .prefixUrl('api')
+  .all('sessions')
+  .getAll()
+  .then(function (response) {
+    console.log(response.body().map(session => session.data()));
+  });
 
 React.render(content, document.body);
