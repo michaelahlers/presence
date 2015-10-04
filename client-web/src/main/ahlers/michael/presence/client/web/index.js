6fd8f2aa48a37dd3eb615759f277d03b489cc019
Michael Ahlers <michael@ahlers.co>
2015-10-04 10:14:51 -0400

    Load settings before rendering the root component.

--- client-web/src/main/ahlers/michael/presence/client/web/index.js
+++ client-web/src/main/ahlers/michael/presence/client/web/index.js
@@ -5,19 +5,23 @@ require('./index.less');
 import React from 'react';
 import restful from 'restful.js';
 
+import settings from './settings';
+
 const content =
   <div>
     <h1>Presence</h1>
-    <p>Hello world!</p>
   </div>;
 
-restful('localhost')
-  .port(9000)
-  .prefixUrl('api')
-  .all('sessions')
-  .getAll()
-  .then(function (response) {
-    console.log(response.body().map(session => session.data()));
-  });
+//restful('localhost')
+//  .port(9000)
+//  .prefixUrl('api')
+//  .all('sessions')
+//  .getAll()
+//  .then(function (response) {
+//    console.log(response.body().map(session => session.data()));
+//  });
 
-React.render(content, document.body);
+require('./settings').then(settings => {
+  console.log('settings', settings);
+  React.render(content, document.body);
+});
