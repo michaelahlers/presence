ebedbc607cdcdeece467c95a3c492e11d5fa0566
Michael Ahlers <michael@ahlers.co>
2015-10-12 19:52:56 -0400

    Clean up.

--- client-web/src/ahlers/michael/presence/client/web/index.js
+++ client-web/src/ahlers/michael/presence/client/web/index.js
@@ -3,25 +3,12 @@
 require('./index.less');
 
 import React from 'react';
-import restful from 'restful.js';
-
-import settings from './settings';
 
 const content =
   <div>
     <h1>Presence</h1>
   </div>;
 
-//restful('localhost')
-//  .port(9000)
-//  .prefixUrl('api')
-//  .all('sessions')
-//  .getAll()
-//  .then(function (response) {
-//    console.log(response.body().map(session => session.data()));
-//  });
-
 require('./settings').then(settings => {
-  console.log('settings', settings);
   React.render(content, document.body);
 });
