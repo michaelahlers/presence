ca9f9d1f9e2ae42d7c892d76ba901ec198a6e1a1
Michael Ahlers <michael@ahlers.co>
2016-01-22 18:07:54 -0500

    Add hack to get browser history working on S3

--- client-browser/src/index.js
+++ client-browser/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { createHistory } from 'history';
+
 import {Router, browserHistory} from 'react-router';
 
 const route = {
@@ -29,7 +31,17 @@ const route = {
 
 };
 
+/* In light of http://stackoverflow.com/questions/16267339/s3-static-website-hosting-route-all-paths-to-index-html forgo using HTML5 browser history until a good solution is found to deploy to S3. See also http://rackt.org/history/stable/HashHistoryCaveats.html for details on (and caveats especially of) hash history. */
+// const history = useBasename(createHistory)({});
+const history = createHistory();
+
+history.listen(function (location) {
+  if (-1 < location.hash.indexOf('#/')) {
+    history.replace(location.hash.substring(1));
+  }
+});
+
 ReactDOM.render(
-  <Router history={browserHistory} routes={route}/>,
+  <Router history={history} routes={route}/>,
   document.body.appendChild(document.createElement('div'))
 );
