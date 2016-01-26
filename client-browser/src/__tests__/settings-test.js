80fae0330c08f316dd40b966456bc6e0e92a41a4
Michael Ahlers <michael@ahlers.co>
2016-01-25 19:55:14 -0500

    Test runtime settings loader

---
+++ client-browser/src/__tests__/settings-test.js
@@ -0,0 +1,57 @@
+jest.dontMock('babel-polyfill');
+jest.dontMock('immutable');
+jest.dontMock('../settings');
+
+/** See http://stackoverflow.com/a/34195553/700420. */
+/* TODO: Document this more broadly. */
+require('babel-polyfill');
+const Immutable = require('immutable');
+
+describe('Settings', () => {
+
+  beforeEach(() => {
+    jasmine.addMatchers({
+      is(expected) {
+        return Immutable.is(this.actual, expected)
+      }
+    });
+  });
+
+  describe('loader', () => {
+
+    it('overrides defaults with deployment settings', async () => {
+      require('whatwg-fetch').setResponse({
+        status: 200,
+        json: function () {
+          return {
+            'api': {
+              'host': 'production-server'
+            }
+          }
+        }
+      });
+
+      const settings = await require('../settings.js');
+
+      expect(settings).is(Immutable.Map({
+        'api': {
+          'host': 'production-server',
+          'port': 9000
+        }
+      }));
+    });
+
+    it('falls back on defaults on request error', async () => {
+      const settings = await require('../settings.js');
+
+      expect(settings).is(Immutable.Map({
+        'api': {
+          'host': 'localhost',
+          'port': 9000
+        }
+      }));
+    });
+
+  });
+
+});
