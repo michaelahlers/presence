770c5ea776413a5abf04b1d9d035ff472e381d79
Michael Ahlers <michael@ahlers.consulting>
2020-10-06 09:45:48 -0400

    Add a stub main class

---
+++ modules/web-client/src/main/scala/ahlers/presence/web/client/WebClientApplication.scala
@@ -0,0 +1,10 @@
+package ahlers.presence.web.client
+
+/**
+ * @author <a href="michael@ahlers.consulting">Michael Ahlers</a>
+ * @since October 05, 2020
+ */
+object WebClientApplication {
+  def main(arguments: Array[String]): Unit =
+    ???
+}
