c87e90b80b75c804aa1c5bcda885af1f8d11bdf4
Michael Ahlers <michael@ahlers.consulting>
2020-11-23 15:31:40 -0500

    Add plugin

--- modules/web-client/src/main/scala/ahlers/presence/web/client/WebApplication.scala
+++ modules/web-client/src/main/scala/ahlers/presence/web/client/WebApplication.scala
@@ -1,7 +1,14 @@
 package ahlers.presence.web.client
 
-import ahlers.presence.web.Message
+import com.thoughtworks.binding.Binding
+import Binding._
+import org.lrng.binding.html
+import html.NodeBinding
+import org.scalajs.dom.html.{ Input, Table }
+import org.scalajs.dom.raw._
 import slogging.{ HttpLoggerFactory, LazyLogging, LoggerConfig }
+
+import scala.concurrent.Future
 
 /**
  * @author <a href="michael@ahlers.consulting">Michael Ahlers</a>
@@ -10,7 +17,64 @@ import slogging.{ HttpLoggerFactory, LazyLogging, LoggerConfig }
 object WebApplication extends LazyLogging {
   LoggerConfig.factory = HttpLoggerFactory("/logging")
 
+  case class Contact(name: Var[String], email: Var[String])
+
+  val contacts = Vars.empty[Contact]
+
+  @html val inputName: NodeBinding[Input] = <input type="text" />
+
+  @html val inputEmail: NodeBinding[Input] = <input type="text" />
+
+  @html
+  val table =
+    <div>
+
+    {inputName.bind}
+    {inputEmail.bind}
+
+      <button
+      onclick={
+      event: Event =>
+        val name: Var[String] = Var("Michael Ahlers")
+        val email: Var[String] = Var("michael@ahlers.consulting")
+
+        name.value = inputName.value.value
+        email.value = inputEmail.value.value
+
+        val contact = Contact(name, email)
+        logger.info(s"""Added contact ${contact.name.value}.""")
+        contacts.value += contact
+    }>
+        Add a contact
+      </button>
+    </div>
+
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>E-mail</th>
+        </tr>
+      </thead>
+      <tbody>
+        {
+      for (contact <- contacts)
+        yield <tr>
+            <td>
+              {contact.name.bind}
+            </td>
+            <td>
+              {contact.email.bind}
+            </td>
+          </tr>
+    }
+      </tbody>
+    </table>
+
   def main(arguments: Array[String]): Unit =
     logger.info("Hello, World!")
+
+  //import org.scalajs.dom.document
+  //html.render(document.body, table)
 
 }
