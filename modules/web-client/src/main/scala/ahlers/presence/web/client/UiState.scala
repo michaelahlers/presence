9a79d5102f294f8fd9bf5b50ca7777ccfe7b38ed
Michael Ahlers <michael@ahlers.consulting>
2020-11-28 15:41:39 -0500

    Start formalizing UI state

---
+++ modules/web-client/src/main/scala/ahlers/presence/web/client/UiState.scala
@@ -0,0 +1,41 @@
+package ahlers.presence.web.client
+
+import com.raquo.laminar.api.L._
+import com.raquo.waypoint.{ endOfSegments, root, Route, Router }
+import org.scalajs.dom.window
+import io.circe._
+import io.circe.generic.auto._
+import io.circe.parser._
+import io.circe.syntax._
+import cats.syntax.either._
+
+import scala.util.control.NoStackTrace
+
+/**
+ * @since November 28, 2020
+ * @author <a href="mailto:michael@ahlers.consulting">Michael Ahlers</a>
+ */
+sealed trait UiState
+object UiState {
+
+  case object Landing extends UiState
+  case object Resume extends UiState
+  case object Contact extends UiState
+
+  val landingRoute = Route.static(Landing, root / endOfSegments)
+  val resumeRoute = Route.static(Resume, root / "resume" / endOfSegments)
+  val contactRoute = Route.static(Contact, root / "contact" / endOfSegments)
+
+  object router
+    extends Router[UiState](
+      initialUrl = window.location.href,
+      origin = window.location.origin.get,
+      routes = List(landingRoute, resumeRoute, contactRoute),
+      owner = unsafeWindowOwner,
+      $popStateEvent = windowEvents.onPopState,
+      getPageTitle = _.toString,
+      serializePage = _.asJson.noSpaces,
+      deserializePage = decode[UiState](_).valueOr(throw _)
+    )
+
+}
--- modules/web-client/src/main/scala/ahlers/presence/web/client/WebClientApplication.scala
+++ modules/web-client/src/main/scala/ahlers/presence/web/client/WebClientApplication.scala
@@ -2,7 +2,6 @@ package ahlers.presence.web.client
 
 import com.raquo.laminar.api.L._
 import org.scalajs.dom
-import org.scalajs.dom.window
 import slogging.{ HttpLoggerFactory, LazyLogging, LoggerConfig }
 
 /**
@@ -11,42 +10,6 @@ import slogging.{ HttpLoggerFactory, LazyLogging, LoggerConfig }
  */
 object WebClientApplication extends LazyLogging {
   LoggerConfig.factory = HttpLoggerFactory("/logs")
-
-  sealed trait Page
-  case object HomePage extends Page
-  case object WorkPage extends Page
-  case object CompanyPage extends Page
-  case object CareersPage extends Page
-
-  val router = {
-    import com.raquo.waypoint._
-
-    val homeRoute = Route.static(HomePage, root / "home" / endOfSegments)
-    val workRoute = Route.static(WorkPage, root / "work" / endOfSegments)
-    val companyRoute = Route.static(CompanyPage, root / "company" / endOfSegments)
-    val careersRoute = Route.static(CareersPage, root / "careers" / endOfSegments)
-
-    new Router[Page](
-      initialUrl = window.location.href,
-      origin = window.location.origin.get,
-      routes = List(homeRoute, workRoute, companyRoute, careersRoute),
-      owner = unsafeWindowOwner,
-      $popStateEvent = windowEvents.onPopState,
-      getPageTitle = _.toString,
-      serializePage = {
-        case HomePage => "home"
-        case WorkPage => "work"
-        case CompanyPage => "company"
-        case CareersPage => "careers"
-      },
-      deserializePage = {
-        case "home" => HomePage
-        case "work" => WorkPage
-        case "company" => CompanyPage
-        case "careers" => CareersPage
-      }
-    )
-  }
 
   def main(arguments: Array[String]): Unit = {
 
@@ -64,11 +27,11 @@ object WebClientApplication extends LazyLogging {
 //  .sidebar("attach events", ".toc.item")
 
     def menu = {
-      def link(page: Page, label: String) =
+      def link(page: UiState, label: HtmlElement) =
         a(
           className := "item",
-          onClick.preventDefault.mapToValue(page) --> (router.pushState(_)),
-          href := router.relativeUrlForPage(page),
+          onClick.preventDefault.mapToValue(page) --> (UiState.router.pushState(_)),
+          href := UiState.router.relativeUrlForPage(page),
           label)
 
       div(
@@ -77,19 +40,9 @@ object WebClientApplication extends LazyLogging {
           className := "ui container",
           div(
             className := "ui large secondary inverted pointing menu",
-            link(HomePage, "Home"),
-            link(WorkPage, "Work"),
-            link(CompanyPage, "Company"),
-            link(CareersPage, "Careers"),
-            div(
-              className := "right item",
-              a(
-                className := "ui inverted button",
-                "Log In"),
-              a(
-                className := "ui inverted button",
-                "Sign Up")
-            )
+            link(UiState.Landing, i(className := "home icon")),
+            link(UiState.Resume, span("Resume")),
+            link(UiState.Contact, span("Contact"))
           )
         )
       )
