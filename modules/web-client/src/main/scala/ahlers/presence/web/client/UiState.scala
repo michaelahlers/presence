537a0e935a713466fb56cce1ee350858c32afa1c
Michael Ahlers <michael@ahlers.consulting>
2020-11-28 15:52:07 -0500

    Log the UI state

--- modules/web-client/src/main/scala/ahlers/presence/web/client/UiState.scala
+++ modules/web-client/src/main/scala/ahlers/presence/web/client/UiState.scala
@@ -1,22 +1,21 @@
 package ahlers.presence.web.client
 
+import cats.syntax.either._
 import com.raquo.laminar.api.L._
 import com.raquo.waypoint.{ endOfSegments, root, Route, Router }
-import org.scalajs.dom.window
-import io.circe._
 import io.circe.generic.auto._
 import io.circe.parser._
 import io.circe.syntax._
-import cats.syntax.either._
-
-import scala.util.control.NoStackTrace
+import org.scalajs.dom.window
+import slogging.{ HttpLoggerFactory, LazyLogging, LoggerConfig }
 
 /**
  * @since November 28, 2020
  * @author <a href="mailto:michael@ahlers.consulting">Michael Ahlers</a>
  */
 sealed trait UiState
-object UiState {
+object UiState extends LazyLogging {
+  LoggerConfig.factory = HttpLoggerFactory("/logs")
 
   case object Landing extends UiState
   case object Resume extends UiState
@@ -37,5 +36,11 @@ object UiState {
       serializePage = _.asJson.noSpaces,
       deserializePage = decode[UiState](_).valueOr(throw _)
     )
+
+  router
+    .$currentPage
+    .foreach(state =>
+      logger
+        .info(s"""Transitioned to state ${state.getClass()}."""))(unsafeWindowOwner)
 
 }
