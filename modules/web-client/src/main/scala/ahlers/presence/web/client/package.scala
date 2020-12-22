0903a5c54d3c72e9d9ed67e0e30704cea98ee3b0
Michael Ahlers <michael@ahlers.consulting>
2020-12-22 16:57:19 -0500

    Dabbling with Scala CSS

---
+++ modules/web-client/src/main/scala/ahlers/presence/web/client/package.scala
@@ -0,0 +1,10 @@
+package ahlers.presence.web
+
+/**
+ * @since December 22, 2020
+ * @author <a href="mailto:michael@ahlers.consulting">Michael Ahlers</a>
+ */
+package object client {
+  // TODO: Initialize from Play's environment mode.
+  val CssSettings = scalacss.devOrProdDefaults
+}
