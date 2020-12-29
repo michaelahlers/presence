ffb19a819f90c01614bfc0a8efea01a0aada05d1
Michael Ahlers <michael@ahlers.consulting>
2020-12-29 16:20:56 -0500

    Improve layout across sizes

--- modules/web-client/src/main/scala/ahlers/presence/web/client/LandingPageView.scala
+++ modules/web-client/src/main/scala/ahlers/presence/web/client/LandingPageView.scala
@@ -23,19 +23,19 @@ object LandingPageView {
       div(
         className := "row justify-content-center mt-5",
         div(
-          className := "col-4",
+          className := "col-xl-3 col-lg-4 col-md-5 col-9",
           img(src := Asset.versioned("ahlers/presence/web/client/Ahlers Consulting (dark).svg").url)),
         div(
-          className := "col-5 text-nowrap",
+          className := "col-xl-3 col-lg-4 col-md-5 col-6 mt-md-auto mt-4",
           h1(className := "title", "Michael Ahlers"),
           //h2(className := "text-lowercase text-muted", "Software Engineer"),
           ContactInformationView()
         )
       ),
       div(
-        className := "row justify-content-center mt-5 mb-3",
+        className := "row justify-content-center mt-md-5 mt-4 mb-3",
         div(
-          className := "col-8",
+          className := "col-xl-7 col-lg-8 col-md-9 col-12",
           p(
             className := "lead",
             child.text <--
