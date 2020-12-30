df7a16272fac1d5a79ea6fa2e6c9850d06f48cc2
Michael Ahlers <michael@ahlers.consulting>
2020-12-30 12:54:57 -0500

    Adjust styling

--- modules/web-client/src/main/scala/ahlers/presence/web/client/ContactInformationView.scala
+++ modules/web-client/src/main/scala/ahlers/presence/web/client/ContactInformationView.scala
@@ -10,15 +10,15 @@ object ContactInformationView {
 
   def apply(): HtmlElement = {
     val referenceLabelAnchors: Seq[(String, HtmlElement, HtmlElement)] =
-      ("mailto:michael@ahlers.consulting", i(className := "fas fa-envelope-square"), span(className := "ms-1", "michael@ahlers.consulting")) ::
-        ("http://linkedin.com/in/michaelahlers", i(className := "fab fa-linkedin"), span(className := "ms-1", "/in/michaelahlers")) ::
-        ("http://github.com/michaelahlers", i(className := "fab fa-github-square"), span(className := "ms-1", "/michaelahlers")) ::
-        ("http://stackexchange.com/users/359179/michael-ahlers", i(className := "fab fa-stack-exchange"), span(className := "ms-1", "/users/359179/michael-ahlers")) ::
-        ("tel:+1-571-830-0258", i(className := "fas fa-phone-square"), span(className := "ms-1", "+1 (571) 830-0258")) ::
+      ("mailto:michael@ahlers.consulting", i(className := "fas fa-envelope-square"), span(className := "ms-2", "michael@ahlers.consulting")) ::
+        ("http://linkedin.com/in/michaelahlers", i(className := "fab fa-linkedin"), span(className := "ms-2", "/in/michaelahlers")) ::
+        ("http://github.com/michaelahlers", i(className := "fab fa-github-square"), span(className := "ms-2", "/michaelahlers")) ::
+        ("http://stackexchange.com/users/359179/michael-ahlers", i(className := "fab fa-stack-exchange"), span(className := "ms-2", "/users/359179/michael-ahlers")) ::
+        ("tel:+1-571-830-0258", i(className := "fas fa-phone-square"), span(className := "ms-2", "+1 (571) 830-0258")) ::
         Nil
 
     ul(
-      className := "list-unstyled text-nowrap",
+      className := "list-unstyled lead",
       referenceLabelAnchors.map {
         case (reference, label, anchor) =>
           li(
