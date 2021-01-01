019499d321b91b0c410f88dead7ccc5775f5e73a
Michael Ahlers <michael@ahlers.consulting>
2021-01-01 14:20:17 -0500

    Tweak layout and borrow navigation styles for contact info. links

--- modules/web-client/src/main/scala/ahlers/presence/web/client/ContactInformationView.scala
+++ modules/web-client/src/main/scala/ahlers/presence/web/client/ContactInformationView.scala
@@ -10,11 +10,11 @@ object ContactInformationView {
 
   def apply(): HtmlElement = {
     val referenceLabelAnchors: Seq[(String, HtmlElement, HtmlElement)] =
-      ("mailto:michael@ahlers.consulting", i(className := "fas fa-envelope-square"), span(className := "ms-2", "michael@ahlers.consulting")) ::
-        ("http://linkedin.com/in/michaelahlers", i(className := "fab fa-linkedin"), span(className := "ms-2", "/in/michaelahlers")) ::
-        ("http://github.com/michaelahlers", i(className := "fab fa-github-square"), span(className := "ms-2", "/michaelahlers")) ::
-        ("http://stackexchange.com/users/359179/michael-ahlers", i(className := "fab fa-stack-exchange"), span(className := "ms-2", "/users/359179/michael-ahlers")) ::
-        ("tel:+1-571-830-0258", i(className := "fas fa-phone-square"), span(className := "ms-2", "+1 (571) 830-0258")) ::
+      ("mailto:michael@ahlers.consulting", i(className := "fas fa-envelope-square"), span(className := "ms-1", "michael@ahlers.consulting")) ::
+        ("http://linkedin.com/in/michaelahlers", i(className := "fab fa-linkedin"), span(className := "ms-1", "/in/michaelahlers")) ::
+        ("http://github.com/michaelahlers", i(className := "fab fa-github-square"), span(className := "ms-1", "/michaelahlers")) ::
+        ("http://stackexchange.com/users/359179/michael-ahlers", i(className := "fab fa-stack-exchange"), span(className := "ms-1", "/users/359179/michael-ahlers")) ::
+        ("tel:+1-571-830-0258", i(className := "fas fa-phone-square"), span(className := "ms-1", "(571) 830-0258")) ::
         Nil
 
     ul(
@@ -23,6 +23,7 @@ object ContactInformationView {
         case (reference, label, anchor) =>
           li(
             a(
+              className := "contact-link",
               href := reference,
               label,
               anchor))
