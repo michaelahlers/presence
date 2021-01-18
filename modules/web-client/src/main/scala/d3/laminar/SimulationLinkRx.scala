65f51c929827cd7756e135c9455a75b3472e8357
Michael Ahlers <michael@ahlers.consulting>
2021-01-18 10:45:43 -0500

    Refactor so only immutable features are reactive variables

--- modules/web-client/src/main/scala/d3/laminar/SimulationLinkRx.scala
+++ modules/web-client/src/main/scala/d3/laminar/SimulationLinkRx.scala
@@ -1,52 +1,24 @@
 package d3.laminar
 
-import com.raquo.airstream.signal.Var
 import d3v4.{ Index, SimulationLink, SimulationNode }
 
-import scala.scalajs.js.JSConverters._
-import scala.scalajs.js.UndefOr
+import scala.scalajs.js
 
 /**
  * @since January 03, 2021
  * @author <a href="mailto:michael@ahlers.consulting">Michael Ahlers</a>
  */
-trait SimulationLinkRx[Source <: SimulationNode, Target <: SimulationNode] extends SimulationLink[Source, Target] {
-  def indexVar: Var[Option[Index]]
-  def sourceVar: Var[Source]
-  def targetVar: Var[Target]
+trait SimulationLinkRx[Source <: SimulationNode, Target <: SimulationNode]
+  extends SimulationLink[
+    Source,
+    Target
+  ] {
 
-  final def $index = indexVar.signal
-  final override def index = indexVar.now().orUndefined
-  final override def index_=(index: UndefOr[Index]) = indexVar.set(index.toOption)
-
-  final def $source = sourceVar.signal
-  final override def source = sourceVar.now()
-
-  final def $target = targetVar.signal
-  final override def target = targetVar.now()
-
-}
-
-object SimulationLinkRx {
-
-  //def apply[Source <: SimulationNode, Target <: SimulationNode](
-  //  index: Option[Index],
-  //  source: Source,
-  //  target: Target
-  //): SimulationLinkRx[Source, Target] =
-  //  SimulationLinkRx(
-  //    Var(index),
-  //    Var(source),
-  //    Var(target))
-  //
-  //def apply[Source <: SimulationNode, Target <: SimulationNode](
-  //  index: Index,
-  //  source: Source,
-  //  target: Target
-  //): SimulationLinkRx[Source, Target] =
-  //  SimulationLinkRx(
-  //    index.some,
-  //    source,
-  //    target)
+  // FIXME: Disabled pending Airstream fix.
+  //final val indexVar: Var[Option[Index]] = Var(none)
+  //final val $index: StrictSignal[Option[Index]] = indexVar.signal
+  //final override def index = indexVar.now().orUndefined
+  //final override def index_=(index: UndefOr[Index]) = indexVar.set(index.toOption)
+  final var index: js.UndefOr[Index] = js.undefined
 
 }
