a16f129eb2ca78fb54c867b5cc3f416433e2b355
Michael Ahlers <michael@ahlers.consulting>
2021-01-19 18:32:00 -0500

    Use rich types

--- modules/web-client/src/main/scala/ahlers/presence/web/client/Asset.scala
+++ modules/web-client/src/main/scala/ahlers/presence/web/client/Asset.scala
@@ -1,15 +1,24 @@
 package ahlers.presence.web.client
 
+import io.lemonlabs.uri.{ Path, Url }
+
 import scala.scalajs.js
 
 /**
  * @since December 23, 2020
  * @author <a href="mailto:michael@ahlers.consulting">Michael Ahlers</a>
  */
-case class Asset(url: String, absoluteUrl: String)
+case class Asset(url: Url, absoluteUrl: Url)
 object Asset {
-  def versioned(path: String): Asset = {
-    val request = js.Dynamic.global.jsRoutes.controllers.Assets.versioned(path)
-    Asset(request.url.toString, request.absoluteURL().toString)
+
+  def versioned(path: Path): Asset = {
+    val request = js.Dynamic.global.jsRoutes.controllers.Assets.versioned(path.toString())
+    Asset(
+      Url.parse(request.url.toString()),
+      Url.parse(request.absoluteURL().toString))
   }
+
+  def versioned(path: String): Asset =
+    versioned(Path.parse(path))
+
 }
