4acad4a4faaeae8c0ae3df6325cb550d75e2c42d
Michael Ahlers <michael@ahlers.consulting>
2021-01-31 13:32:18 -0500

    Clean up and formalize with best (better) practices

---
+++ modules/web-client/src/main/scala/ahlers/presence/web/client/resume/ExperienceNodeState.scala
@@ -0,0 +1,10 @@
+package ahlers.presence.web.client.resume
+
+/**
+ * @since January 31, 2021
+ * @author <a href="mailto:michael@ahlers.consulting">Michael Ahlers</a>
+ */
+case class ExperienceNodeState(
+  focused: Boolean,
+  x: Double,
+  y: Double)
