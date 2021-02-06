a4a1e356d34af6d72df8978193308a4205dc9dcc
Michael Ahlers <michael@ahlers.consulting>
2021-02-06 18:35:55 -0500

    Start adding type class instances for Laminar classes

---
+++ modules/web-client/src/main/scala/cats/laminar/instances/eventStream.scala
@@ -0,0 +1,7 @@
+package cats.laminar.instances
+
+/**
+ * @author <a href="mailto:michael@ahlers.consulting">Michael Ahlers</a>
+ * @since February 06, 2021
+ */
+object eventStream extends EventStreamInstances
