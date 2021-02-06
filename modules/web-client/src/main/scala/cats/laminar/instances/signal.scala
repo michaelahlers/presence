7f8681840ed6fd6ff1e36395111abdb4156f0621
Michael Ahlers <michael@ahlers.consulting>
2021-02-06 18:29:01 -0500

    Start adding type class instances for Laminar classes

---
+++ modules/web-client/src/main/scala/cats/laminar/instances/signal.scala
@@ -0,0 +1,7 @@
+package cats.laminar.instances
+
+/**
+ * @author <a href="mailto:michael@ahlers.consulting">Michael Ahlers</a>
+ * @since February 06, 2020
+ */
+object signal extends SignalInstances
