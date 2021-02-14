dc99c3d3faa816b5689cb6069faf43eea44dc884
Michael Ahlers <michael@ahlers.consulting>
2021-02-14 13:45:55 -0500

    Reveal with a pure CSS solution

--- modules/web-client/src/main/scala/ahlers/presence/web/client/resume/ExperienceNodeView.scala
+++ modules/web-client/src/main/scala/ahlers/presence/web/client/resume/ExperienceNodeView.scala
@@ -51,6 +51,7 @@ object ExperienceNodeView {
     g(
       className := Seq("experience-node-view", nodeState.kind),
       className <-- $classNames,
+      style := "--delay: %dms".format(nodeState.index.toInt * 10),
       nodeState.logo match {
         case None =>
           circle(
