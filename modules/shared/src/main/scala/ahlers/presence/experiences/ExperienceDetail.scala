52f0f4cd97ea18cc97c936fb1c7cf4a0fba57456
Michael Ahlers <michael@ahlers.consulting>
2021-02-21 15:03:11 -0500

    Elaborate on details

--- modules/shared/src/main/scala/ahlers/presence/experiences/ExperienceDetail.scala
+++ modules/shared/src/main/scala/ahlers/presence/experiences/ExperienceDetail.scala
@@ -4,15 +4,20 @@ package ahlers.presence.experiences
  * @since February 21, 2021
  * @author <a href="mailto:michael@ahlers.consulting">Michael Ahlers</a>
  */
-sealed trait ExperienceDetail
+sealed trait ExperienceDetail {
+  def logo: ExperienceLogo
+  def name: ExperienceName
+}
 object ExperienceDetail {
 
   case class Employment(
-    logo: ExperienceLogo)
+    logo: ExperienceLogo,
+    name: ExperienceName)
     extends ExperienceDetail
 
   case class Skill(
-    logo: ExperienceLogo)
+    logo: ExperienceLogo,
+    name: ExperienceName)
     extends ExperienceDetail
 
 }
