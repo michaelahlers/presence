a54023289553c01504823d8839facf8b77c9376a
Michael Ahlers <michael@ahlers.consulting>
2021-02-21 09:37:36 -0500

    Clean up and add class name for modal state

--- modules/web-client/src/main/scala/ahlers/presence/web/client/WebClientApplication.scala
+++ modules/web-client/src/main/scala/ahlers/presence/web/client/WebClientApplication.scala
@@ -11,54 +11,6 @@ import slogging.{ HttpLoggerFactory, LazyLogging, LoggerConfig }
 object WebClientApplication extends App with LazyLogging {
   LoggerConfig.factory = HttpLoggerFactory("/logs")
 
-  //GlobalStyles.addToDocument()
-  //GlobalRegistry.addToDocumentOnRegistration()
-
-  //$(".masthead")
-  //  .visibility(SemanticUiVisibilitySettings
-  //    .once(false)
-  //    .onBottomPassed(() =>
-  //      $(".fixed.menu")
-  //        .transition("fade in"))
-  //    .onBottomPassedReverse(() =>
-  //      $(".fixed.menu")
-  //        .transition("fade out")))
-  //
-  //$(".ui.sidebar")
-  //  .sidebar("attach events", ".toc.item")
-
-  case class Datum(name: String, children: Datum*)
-
-  //val data: Datum =
-  //  Datum(
-  //    "Root",
-  //    Datum("Child 1"),
-  //    Datum("Child 2"))
-
-  //val hierarchy: Hierarchy[Datum] =
-  //  d3.hierarchy[Datum](data, (_: Datum).children.toJSArray)
-  //    .each { node: Hierarchy[Datum] =>
-  //      println(s"""name = "%s", depth = %d"""
-  //        .format(
-  //          node.data.name,
-  //          node.depth))
-  //    }
-
-  //d3.pack[Datum]()
-  //  .size(js.Array(200, 100))
-  //  .padding(_ => 2d)
-  //  .radius(_ => 10d)
-  //  .apply(hierarchy)
-  //  .each { node: Hierarchy[Datum] with Packed =>
-  //    println(s"""name = "%s", depth = %d, x = %d, y = %d, r = %d"""
-  //      .format(
-  //        node.data.name,
-  //        node.depth,
-  //        node.x,
-  //        node.y,
-  //        node.r))
-  //  }
-
   /** Bootstrap utility classes can't be assigned to parents using modifiers. */
   Seq("h-100")
     .foreach(dom
@@ -71,7 +23,8 @@ object WebClientApplication extends App with LazyLogging {
   documentEvents
     .onDomContentLoaded
     .mapToValue(body(
-      className := Seq("d-flex", "flex-column", "h-100"),
+      className("d-flex", "flex-column", "h-100"),
+      className.toggle("modal-open") <-- UiState.$modals.map(_.nonEmpty),
       HeaderView(),
       MainView(),
       FooterView()))
