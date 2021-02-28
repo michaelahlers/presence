be941dea2a9a8e784419a45500ce8b4b5c296a28
Michael Ahlers <michael@ahlers.consulting>
2021-02-28 10:07:54 -0500

    Render AST as Laminar reactive elements

--- modules/web-client/src/main/scala/ahlers/presence/web/client/resume/ExperienceFocusView.scala
+++ modules/web-client/src/main/scala/ahlers/presence/web/client/resume/ExperienceFocusView.scala
@@ -4,6 +4,12 @@ import ahlers.presence.experiences.{ Experience, ExperienceKey }
 import cats.syntax.option._
 import com.raquo.domtypes.generic.Modifier
 import com.raquo.laminar.api.L._
+import laika.api.Transformer
+import laika.ast.{ Block, Emphasized, Paragraph, RootElement }
+import laika.format.{ AST, Markdown }
+import laika.markdown.github.GitHubFlavor
+import laika.parse.code.CodeCategory.Markup.Emphasized
+import scala.annotation.tailrec
 
 /**
  * @since February 20, 2021
@@ -15,6 +21,47 @@ object ExperienceFocusView {
     onClick
       .stopPropagation
       .mapToStrict(none)
+
+  val transformer =
+    Transformer
+      .from(Markdown)
+      .to(AST)
+      .using(GitHubFlavor)
+      .build
+
+  implicit class LaikaElementSyntax(private val element: laika.ast.Element) {
+
+    /**
+     * Converts a [[laika Laika]] [[laika.ast.Element]] to a reactive [[Node]].
+     * @todo Formalize and test—this is, oh, so much a work-in-progress proof-of-concept.
+     * @todo Make [[tailrec]]. 😬
+     */
+    def toNode: Node = {
+      import laika.ast._
+      element match {
+        case element: RootElement =>
+          div(element.content.map(_.toNode))
+        case element: Paragraph =>
+          p(element.content.map(_.toNode))
+        case element: Text =>
+          textToNode(element.content)
+        case element: Emphasized =>
+          i(element.content.map(_.toNode))
+        case element: SpanLink =>
+          element.target match {
+            case link: ExternalTarget =>
+              a(
+                href(link.url),
+                target("_blank"),
+                element.content.map(_.toNode))
+            case target: InternalTarget =>
+              commentNode(s"${target.getClass}")
+          }
+        case element =>
+          commentNode(s"${element.getClass}.")
+      }
+    }
+  }
 
   def render(
     experienceKey: ExperienceKey,
@@ -38,7 +85,12 @@ object ExperienceFocusView {
     val bodyRender =
       div(
         className("modal-body"),
-        p(child.text <-- $experience.map(_.detail.name.toText)))
+        child <--
+          $experience
+            .map(_.detail.commentary.toText)
+            .map(transformer.parser.parse(_).getOrElse(???))
+            .map(_.content.toNode)
+      )
 
     val footerRender =
       div(
